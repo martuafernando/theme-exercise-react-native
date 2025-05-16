6809cb8faef8bcae06e884d28be2f68f24b7a523
martuafernando <fernandosibarani45@gmail.com>
2025-05-16 14:16:28 +0700

    feat: add loading when token checking process

--- app/index.tsx
+++ app/index.tsx
@@ -1,18 +1,29 @@
 import { Redirect } from "expo-router";
 import { getAccessToken } from "lib/auth";
 import { useEffect, useState } from "react";
+import { ActivityIndicator, View } from "react-native";
 
 export default function Index() {
 	const [token, setToken] = useState<string | null>(null);
+	const [loading, setLoading] = useState(true);
 
 	useEffect(() => {
 		const getToken = async () => {
 			const currentToken = await getAccessToken();
 			setToken(currentToken);
+			setLoading(false);
 		};
 
 		getToken();
 	}, []);
+
+	if (loading) {
+		return (
+			<View className="flex-1 justify-center items-center">
+				<ActivityIndicator size="large" color="#0000ff" />
+			</View>
+		);
+	}
 
 	return <Redirect href={token ? "/main" : "/auth/login"} />;
 }
